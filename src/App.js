4620a3b9b5b27cb736c40e101712b9793136118d
Precious Akams <preciousakams@yahoo.com>
2023-05-26 04:54:26 +0100

    style updated

--- src/App.js
+++ src/App.js
@@ -26,36 +26,36 @@ function App() {
                 <input type="number" className="form-control placeHolder" placeholder="2412 - 7512 - 3412 - 3456" aria-label="card number" />
               </div>
               <div className="row cvv">
-                <div className="col-6">
+                <div className="col-8">
                   <h5>CVV Number</h5>
-                  <span>Enter the 3 or 4 digit number on the card</span>
+                  <span className="enterDigit">Enter the 3 or 4 digit number on the card</span>
                 </div>
-                <div className="col-6">
+                <div className="col-4">
                   <input type="number" className="form-control cvvInput" placeholder="327" aria-label="cvv number" />
                 </div>
               </div>
               <div className="row expiryDate">
                 <div className="col-6">
                   <h5>Expiry Date</h5>
-                  <span>Enter the expiration date on the card</span>
+                  <span className="enterDigit">Enter the expiration date on the card</span>
                 </div>
-                <div className="col-6 expiryDate row">
+                <div className="col-6 expiryDateDigit row">
                   <div className="col-5">
                     <input type="number" className="form-control expiryMonth" placeholder="09" aria-label="expiry day" />
                   </div>
                   <div className="col-2">
                     <span className="expirySlash">/</span>
                   </div>
-                  <div className="col-5">
+                  <div className="col-5 expiryYearDiv">
                     <input type="number" className="form-control expiryYear" placeholder="2022" aria-label="expiry year" />
                   </div>
                 </div>
                 <div className="password row">
                   <div className="col-6">
                     <h5>Password</h5>
-                    <span>Enter your dynamic password</span>
+                    <span className="enterDigit">Enter your dynamic password</span>
                   </div>
-                  <div className="col-6">
+                  <div className="col-6 passwordInputDiv">
                     <input type="number" className="form-control passwordInput" placeholder="........" aria-label="password" />
                   </div>
                 </div>
@@ -105,36 +105,35 @@ function App() {
                 <p className="greyText">Company</p>
               </div>
               <div className="col-6">
-                <p>
+                <p className="blackText">
                   <img src="/icons/apple.png" alt="apple" className="appleIcon" /> Apple </p>
               </div>
-              <div className="col-6">
+              <div className="col-8">
                 <p className="greyText">Order Number</p>
               </div>
-              <div className="col-6">
-                <p>1266201</p>
+              <div className="col-4">
+                <p className="blackText">1266201</p>
               </div>
               <div className="col-6">
                 <p className="greyText">Product</p>
               </div>
               <div className="col-6">
-                <p>MacBook Air</p>
+                <p className="blackText">MacBook Air</p>
               </div>
               <div className="col-6">
                 <p className="greyText"> Vat (20%)</p>
               </div>
               <div className="col-6">
-                <p>$100.00</p>
+                <p className="blackText">$100.00</p>
               </div>
+              <img src="/icons/pattern.png" alt="design" className="pattern" />
               <div className="col-12">
                 <span className="greyText">You have to pay</span>
               </div>
-              <img src="/icons/pattern.png" alt="design" className="pattern" />
-
-              <div className="col-6 receiptDiv">
-                <p><strong>549</strong>.99 <span className="greyText">USD</span></p>
+              <div className="col-8 receiptDiv">
+                <p><strong className="blackText">549</strong>.99 <span className="greyText">USD</span></p>
               </div>
-              <div className="col-6 invoiceDiv">
+              <div className="col-4 invoiceDiv">
                 <img src="/icons/invoice.png" alt="receipt" className="receipt" />
               </div>
             </div>
